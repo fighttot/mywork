67ce140daf7c339412f05413f9a7603be211360c
fighttot <127477199+fighttot@users.noreply.github.com>
2023-08-18 03:54:17 +0000

    Deploying to gh-pages from @ fighttot/mywork@6f74d8c5e3c2c1e9cb9fd09063384bc335350bfd 🚀

---
+++ assets/FooTer-1e4a18d8.js
@@ -0,0 +1,1 @@
+import{as as e,f as o,h as s,x as t}from"./index-d6fa05ad.js";const c={},n={class:"footer"},r=t("div",null,null,-1),_=[r];function a(i,l){return o(),s("section",n,_)}const f=e(c,[["render",a]]);export{f as F};
