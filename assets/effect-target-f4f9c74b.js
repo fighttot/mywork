ba2f26f601895e8989e6c0739c4bc374e8268967
fighttot <127477199+fighttot@users.noreply.github.com>
2023-08-21 05:18:14 +0000

    Deploying to gh-pages from @ fighttot/mywork@5a79e17d8797257dea0590e7dc34726cb24f9f7e 🚀

---
+++ assets/effect-target-f4f9c74b.js
@@ -0,0 +1,1 @@
+import{g as i}from"./effect-init-7dbc6119.js";function r(a,t){const e=i(t);return e!==t&&(e.style.backfaceVisibility="hidden",e.style["-webkit-backface-visibility"]="hidden"),e}export{r as e};
